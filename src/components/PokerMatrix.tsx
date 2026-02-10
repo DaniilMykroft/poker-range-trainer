c2ec458e8e2acd9944a2bdf81fbf41e5191eecd3
DaniilMykroft <meikindique@gmail.com>
2026-02-11 00:18:34 +0700

    Fix import statement in PokerMatrix.tsx

--- src/components/PokerMatrix.tsx
+++ src/components/PokerMatrix.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect, useCallback } from 'react';
+import { useState, useRef, useEffect, from 'react';
 import { MatrixCell } from '../types/poker';
 import { generatePokerMatrix } from '../utils/pokerHands';
 
